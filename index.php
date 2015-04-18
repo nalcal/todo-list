9e66907c95912be75e0f8278481d9b193fcd0d67
Nalitha Liyanage <nalitha770@gmail.com>
2015-04-17 19:07:47 -0700

    milestone 18

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title>Simple To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
 </head>
 <body>
 	<div class="wrap"> 
@@ -12,7 +14,7 @@
 				$mysqli = new mysqli('localhost', 'root', 'root', 'task');
 				$query = "SELECT = FROM tasks ORDER BY date ASC, time ASC";
 				if($result = $mysqli->query($query)){
-					$numrows = $result->num_rows:
+					$numrows = $result->num_rows;
 					if ($numrows>0){
 						while($row = $result->fetch_assoc()){
 							$task_id = $row['id'];
